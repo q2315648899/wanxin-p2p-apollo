16c6f1609dfeab658ada07d484ed25252768750e
q2315648899 <406972433@qq.com>
2021-07-17 17:11:52 +0800

    万信金融P2P项目实现服务熔断

--- wanxinp2p_apollo/wanxinp2p-uaa-service/src/main/java/cn/itcast/wanxinp2p/uaa/agent/AccountApiAgent.java
+++ wanxinp2p_apollo/wanxinp2p-uaa-service/src/main/java/cn/itcast/wanxinp2p/uaa/agent/AccountApiAgent.java
@@ -2,14 +2,35 @@ package cn.itcast.wanxinp2p.uaa.agent;
 
 import cn.itcast.wanxinp2p.api.account.model.AccountDTO;
 import cn.itcast.wanxinp2p.api.account.model.AccountLoginDTO;
+import cn.itcast.wanxinp2p.common.domain.CommonErrorCode;
 import cn.itcast.wanxinp2p.common.domain.RestResponse;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "account-service")
+@FeignClient(value = "account-service",fallbackFactory = AccountFallbackFactory.class)
 public interface AccountApiAgent {
-
     @PostMapping(value = "/account/l/accounts/session")
     public RestResponse<AccountDTO> login(@RequestBody AccountLoginDTO accountLoginDTO);
+}
+
+@Component
+@Slf4j
+class AccountFallbackFactory implements FallbackFactory<AccountApiAgent> {
+
+    @Override
+    public AccountApiAgent create(Throwable cause) {
+        return new AccountApiAgent() {
+            @Override
+            public RestResponse<AccountDTO> login(AccountLoginDTO accountLoginDTO) {
+                //记录错误原因
+                log.info("fallback; reason was:", cause);
+                //降级回退
+                return new RestResponse<>(CommonErrorCode.E_999995.getCode(),CommonErrorCode.E_999995.getDesc());
+            }
+        };
+    }
 }
